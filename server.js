72b38c70b1212240ce6a9e084b7db984ea742957
aniket-yadav <aniket.y@somaiya.edu>
2018-02-18 18:35:06 +0530

    update server.js

--- server.js
+++ server.js
@@ -5,6 +5,108 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+  var articles={
+    'article-four':{
+	title:'Article-four|Aniket yadav',
+	heading:'Article-four',
+	date:'20-Feb-2017',
+	content:`
+	<p>
+	This is article-four.this is article-four.
+	This is article-four.this is article-four.
+	This is article-four.this is article-four.
+    </p>
+    <p>
+	this is article-four.this is article-four.
+	this is article-four.this is article-four.
+	this is article-four.this is article-four.
+    </p>`
+},
+   'article-five':{
+	title:'Article-five|Aniket yadav',
+	heading:'Article-five',
+	date:'21-Feb-2017',
+	content:`
+	<p>
+	this is Article-five.this is Article-five.
+	this is article-five.this is Article-five.
+	this is article-five.this is Article-five.
+    </p>
+    <p>
+	this is article-five.this is article-five.
+	this is article-five.this is article-five.
+	this is article-five.this is article-five.
+    </p>`
+},
+'article-six':{
+	title:'Article-six|Aniket yadav',
+	heading:'Article-six',
+	date:'22-Feb-2017',
+	content:`
+	<p>
+	this is Article-six.this is Article-six.
+	this is Article-six.this is Article-six.
+	this is Article-six.this is Article-six.
+    </p>
+    <p>
+	this is Article-six.this is Article-six.
+	this is Article-six.this is Article-six.
+	this is Article-six.this is Article-six.
+    </p>`
+}
+};
+
+
+function createHTMLTemplate(data){
+	var title=data.title;
+	var heading=data.heading;
+	var date=data.date;
+	var content=data.content;
+	var HTMLTemplate=`
+	<!DOCTYPE html>
+    <html>
+    <head>
+	   <title>${title}</title>
+	   <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body class="article">
+	   <h1><a href="/">Home</a></h1>
+	   <hr>
+       <h3>
+         ${heading}
+       </h3>
+       <h4>
+         ${date}
+       </h4>
+       <div> 
+         ${content}
+       </div>
+    </body>
+    </html>`;
+    return HTMLTemplate;
+}
+
+app.get('/:articleName',function(req,res){
+	var articleName=req.params.articleName;
+	res.send(createHTMLTemplate(articles[articleName]));
+
+});
+
+
+/*
+app.get('/article-one',function(req,res){
+	res.sendFile(path.join(__dirname,'ui','article-one.html'));
+});
+
+app.get('/article-two',function(req,res){
+	res.sendFile(path.join(__dirname,'ui','article-two.html'));
+});
+
+app.get('/article-three',function(req,res){
+	res.sendFile(path.join(__dirname,'ui','article-three.html'));
+});
+*/
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -21,7 +123,7 @@ app.get('/ui/aniket.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8080;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
