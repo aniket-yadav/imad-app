1056c91742cfc9de305dd2b5487c7310089c8156
Aniket Yadav <30414261+aniket-yadav@users.noreply.github.com>
2018-02-19 17:09:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,6 +66,8 @@ function createHTMLTemplate(data){
 	<!DOCTYPE html>
     <html>
     <head>
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width" initial-scale='1'>
 	   <title>${title}</title>
 	   <link href="/ui/style.css" rel="stylesheet" />
     </head>
